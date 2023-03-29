124299ded9ce4fdc7ab16cc47412fca056abf320
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-29 10:30:53 +0300

    update state.py with cities method

--- models/state.py
+++ models/state.py
@@ -4,6 +4,9 @@ from models.base_model import BaseModel, Base
 # import sqlalchemy modules
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -12,8 +15,26 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
+    storage = getenv('HBNB_TYPE_STORAGE')
     __tablename__ = "states"
+    if storage == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                              backref="state")
+    else:
+        name = ""
 
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if storage != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
