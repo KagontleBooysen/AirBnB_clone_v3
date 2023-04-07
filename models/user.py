8ce49c53bc2d08141b8e58f28bbfdcf60b9b183a
Wubeshet Yimam <w.yimam@alustudent.com>
2023-04-07 21:36:08 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -7,14 +8,23 @@ import os
 import hashlib
 
 
+def hash_password(password):
+    """hashes a password
+
+    Args:
+        password (_type_): password to hash
+
+    Returns:
+        _type_: _description_
+    """
+    hash_object = hashlib.md5()
+    hash_object.update(password.encode('utf-8'))
+    return hash_object.hexdigest()
+
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
-    # check storage
-    # storage = os.getenv('HBNB_TYPE_STORAGE')
-    # if storage == 'db':
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.storage_type == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
@@ -31,10 +41,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        # if password is provided, hash it to the password
         if self.password:
-            self.password = self.hash_password(self.password)
-
-    def hash_password(self, password):
-        hash_object = hashlib.md5()
-        hash_object.update(password.encode('utf-8'))
-        return hash_object.hexdigest()
+            self.password = hash_password(self.password)
